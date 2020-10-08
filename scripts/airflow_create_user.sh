98b91b749c483e1cb05a984c1bf3e350489f1a1a
Avik Datta <reach4avik@yahoo.com>
2020-10-08 01:25:44 +0100

    added docker cmd for create user

---
+++ scripts/airflow_create_user.sh
@@ -0,0 +1,6 @@
+docker run -it --rm \
+--env-file /home/igf/airflow_test/secrets/airflow_env \
+-v /home/igf/airflow_test/logs:/rds/general/user/igf/home/data2/airflow_test/logs:z \
+-v /home/igf/airflow_test/github/igf-airflow-hpc:/rds/general/user/igf/home/data2/airflow_test/github/igf-airflow-hpc:z \
+-v /home/igf/airflow_test/plugin:/rds/general/user/igf/home/data2/airflow_test/plugin:z \
+apache/airflow:1.10.12 create_user --username USERNAME --firstname FIRSTNAME --lastname LASTNAME --role Admin --email EMAIL --password PASSWORD
