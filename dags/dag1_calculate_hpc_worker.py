ee028da25c7829b1a93a3ce18670a480000aef34
Avik Datta <reach4avik@yahoo.com>
2020-10-09 10:37:09 +0100

    updated hpc script path in dag1

--- dags/dag1_calculate_hpc_worker.py
+++ dags/dag1_calculate_hpc_worker.py
@@ -96,9 +96,9 @@ with dag:
       queue='igf-lims',
       command="""
       {% if ti.xcom_pull(key=params.job_name,task_ids="calculate_new_worker_size_and_branch" ) > 1 %}
-        echo "qsub -V -N {{ params.job_name }} -J 1-{{ ti.xcom_pull(key=params.job_name,task_ids="calculate_new_worker_size_and_branch" ) }}  {{ params.pbs_resource }} -- /script/airflow_worker.sh {{  params.airflow_queue }} {{ params.job_name }}" 
+        echo "qsub -V -N {{ params.job_name }} -J 1-{{ ti.xcom_pull(key=params.job_name,task_ids="calculate_new_worker_size_and_branch" ) }}  {{ params.pbs_resource }} -- /project/tgu/data2/airflow_test/github/igf-airflow-hpc/scripts/hpc/airflow_worker.sh {{  params.airflow_queue }} {{ params.job_name }}"
       {% else %}
-        echo "qsub -V -N {{ params.job_name }} {{ params.pbs_resource }} -- /script/airflow_worker.sh {{  params.airflow_queue }} {{ params.job_name }}"
+        echo "qsub -V -N {{ params.job_name }} {{ params.pbs_resource }} -- /project/tgu/data2/airflow_test/github/igf-airflow-hpc/scripts/hpc/airflow_worker.sh {{  params.airflow_queue }} {{ params.job_name }}"
       {% endif %}
       """,
       params={'pbs_resource':pbs_resource,'airflow_queue':airflow_queue,'job_name':q}
