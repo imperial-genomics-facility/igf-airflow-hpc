9d02977982e13824f3d98b242e1eecce7d5ba0b4
Avik Datta <reach4avik@yahoo.com>
2020-10-09 15:12:20 +0100

    aded test dag for hpc

---
+++ dags/test_dag.py
@@ -0,0 +1,33 @@
+from airflow.models import DAG,Variable
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner':'airflow',
+    'start_date':days_ago(2),
+    'provide_context': True,
+}
+
+dag = DAG(
+        dag_id='test_dag',
+        schedule_interval=None,
+        default_args=args,
+        tags=['test','hpc',]
+      )
+
+with dag:
+  task1 = \
+    BashOperator(
+      task_id='task1',
+      dag=dag,
+      bash_command='echo "Test1"',
+      queue='hpc_4G'
+    )
+  task2 = \
+    BashOperator(
+      task_id='task2',
+      dag=dag,
+      bash_command='echo "Test2"',
+      queue='hpc_1G'
+    )
+  task1 >> task2
