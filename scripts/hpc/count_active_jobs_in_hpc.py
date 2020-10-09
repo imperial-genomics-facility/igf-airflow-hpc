3f233735bb0cbc0b7d68b5e2d4bfd44f698b8817
Avik Datta <reach4avik@yahoo.com>
2020-10-09 15:29:57 +0100

    updates for hpc joc count script

--- scripts/hpc/count_active_jobs_in_hpc.py
+++ scripts/hpc/count_active_jobs_in_hpc.py
@@ -2,6 +2,6 @@ import json
 from igf_airflow.hpc_queue import get_pbspro_job_count
 
 if __name__=='__main__':
-  hpc_jobs = get_pbspro_job_count(job_name_prefix='')
+  hpc_jobs = get_pbspro_job_count(job_name_prefix='hpc')
   hpc_jobs = json.dumps(hpc_jobs)
   print(hpc_jobs)
