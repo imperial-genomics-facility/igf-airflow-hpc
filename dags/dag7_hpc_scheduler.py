88e816aedb0dde7691b0f96b83da7bb6ed701807
Avik Datta <avik.datta@imperial.ac.uk>
2021-02-02 08:40:45 +0000

    added task for restarting airflow flower container

--- dags/dag7_hpc_scheduler.py
+++ dags/dag7_hpc_scheduler.py
@@ -23,23 +23,38 @@ dag = \
     catchup=False,
     schedule_interval="*/30 * * * *",
     max_active_runs=1,
-    tags=['igf-lims'],
+    tags=['igf-lims','hpc'],
     default_args=default_args)
 
 ## SSH HOOK
 hpc_hook = SSHHook(ssh_conn_id='hpc_conn')
 
+igf_lims_ssh_hook = \
+  SSHHook(
+    key_file=Variable.get('hpc_ssh_key_file'),
+    username=Variable.get('hpc_user'),
+    remote_host=Variable.get('igf_lims_server_hostname'))
+
 with dag:
   ## TASK
   run_hpc_scheduler = \
     SSHOperator(
-      task_id = 'run_hpc_scheduler',
-      dag = dag,
-      ssh_hook = hpc_hook,
+      task_id='run_hpc_scheduler',
+      dag=dag,
+      ssh_hook=hpc_hook,
       queue='igf-lims',
-      command = """
+      command="""
         source /etc/bashrc; \
         qsub /project/tgu/data2/airflow_test/github/data-management-python/scripts/hpc/run_hpc_scheduler.sh """)
 
+  ## TASK
+  restart_flower_server = \
+    SSHOperator(
+      task_id='restart_flower_server',
+      dag=dag,
+      ssh_hook=igf_lims_ssh_hook,
+      queue='hpc_4G',
+      command="docker restart airflow_flower")
+
   ## PIPELNE
-  run_hpc_scheduler
+  run_hpc_scheduler > restart_flower_server
