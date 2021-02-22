6ec79cdc6a7aeb2973bdc56e6969383e66971cac
Avik Datta <avik.datta@imperial.ac.uk>
2021-02-22 01:46:49 +0000

    added interop notebook runner

--- dags/dag8_copy_ongoing_seqrun.py
+++ dags/dag8_copy_ongoing_seqrun.py
@@ -15,6 +15,7 @@ from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import reset_manifest_file
 from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import get_seqrun_chunks
 from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import copy_seqrun_chunk
 from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import run_interop_dump
+from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import generate_interop_report_func
 from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import check_progress_for_run_func
 from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import samplesheet_validation_and_branch_func
 from igf_airflow.utils.dag8_copy_ongoing_seqrun_utils import run_tile_demult_list_func
@@ -171,6 +172,23 @@ with dag:
         python_callable=run_interop_dump)
     ## PIPELINE
     wait_for_copy_chunk >> create_interop_dump
+    ## TASK
+    generate_interop_report = \
+      PythonOperator(
+        task_id='generate_interop_report_run_{0}'.format(i),
+        dag=dag,
+        queue='hpc_4G',
+        params={'run_index_number':i,
+                'seqrun_id_pull_key':'ongoing_seqruns',
+                'seqrun_id_pull_task_ids':'generate_seqrun_list',
+                'runInfo_xml_file_name':'RunInfo.xml',
+                'interop_dump_pull_task':'create_interop_dump_run_{0}'.format(i),
+                'timeout':1200,
+                'kernel_name':'python3',
+                'output_notebook_key':'interop_notebook'},
+        python_callable=generate_interop_report_func)
+    ## PIPELINE
+    create_interop_dump >> generate_interop_report
     ## TASK
     check_progress_for_run = \
       BranchPythonOperator(
