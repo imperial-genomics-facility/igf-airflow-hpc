3d989592b69e25b78365c200d0c277fcfc7c1097
Avik Datta <avik.datta@imperial.ac.uk>
2021-10-22 20:02:35 +0100

    added refgenome building methods

--- dags/dag17_create_transcriptome_ref.py
+++ dags/dag17_create_transcriptome_ref.py
@@ -7,6 +7,11 @@ from airflow.operators.python_operator import BranchPythonOperator
 from airflow.operators.bash_operator import BashOperator
 from igf_airflow.utils.dag17_create_transcriptome_ref_utils import download_gtf_file_func
 from igf_airflow.utils.dag17_create_transcriptome_ref_utils import create_star_index_func
+from igf_airflow.utils.dag17_create_transcriptome_ref_utils import create_rsem_index_func
+from igf_airflow.utils.dag17_create_transcriptome_ref_utils import create_reflat_index_func
+from igf_airflow.utils.dag17_create_transcriptome_ref_utils import create_ribosomal_interval_func
+from igf_airflow.utils.dag17_create_transcriptome_ref_utils import create_cellranger_ref_func
+
 
 args = {
     'owner': 'airflow',
@@ -54,3 +59,62 @@ with dag:
                     '--sjdbOverhang', 149]
             },
             python_callable=create_star_index_func)
+    ## TASK
+    create_rsem_index = \
+        PythonOperator(
+            task_id="create_rsem_index",
+            dag=dag,
+            queue='hpc_8G8t',
+            params={
+                'gtf_xcom_task': 'download_gtf_file',
+                'gtf_xcom_key': 'gtf_file',
+                'rsem_ref_xcom_key': 'rsem_ref',
+                'threads': 8
+            },
+            python_callable=create_rsem_index_func)
+    ## TASK
+    create_reflat_index = \
+        PythonOperator(
+            task_id='create_reflat_index',
+            dag=dag,
+            queue='hpc_4G',
+            params={
+                'gtf_xcom_task': 'download_gtf_file',
+                'gtf_xcom_key': 'gtf_file',
+                'refflat_ref_xcom_key': 'refflat_ref'
+            },
+            python_callable=create_reflat_index_func)
+    ## TASK
+    create_ribosomal_interval = \
+        PythonOperator(
+            task_id='create_ribosomal_interval',
+            dag=dag,
+            queue='hpc_4G',
+            params={
+                'gtf_xcom_task': 'download_gtf_file',
+                'gtf_xcom_key': 'gtf_file',
+                'ribosomal_ref_xcom_key': 'ribosomal_ref',
+                'skip_gtf_rows': 5
+            },
+            python_callable=create_ribosomal_interval_func)
+    ## TASK
+    create_cellranger_ref = \
+        PythonOperator(
+            task_id='create_cellranger_ref',
+            dag=dag,
+            queue='hpc_8G8t',
+            params={
+                'gtf_xcom_task': 'download_gtf_file',
+                'gtf_xcom_key': 'gtf_file',
+                'cellranger_ref_xcom_key': 'cellranger_ref',
+                'skip_gtf_rows': 5,
+                'threads': 8,
+                'memory': 8
+            },
+            python_callable=create_cellranger_ref_func)
+    ## PIPELINE
+    download_gtf_file >> create_star_index
+    download_gtf_file >> create_rsem_index
+    download_gtf_file >> create_reflat_index
+    download_gtf_file >> create_ribosomal_interval
+    download_gtf_file >> create_cellranger_ref
