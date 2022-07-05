582c3f04f1c2f9da0e25a9b3640c6cf7ac5a61d3
Avik Datta <avik.datta@imperial.ac.uk>
2022-07-05 13:14:51 +0100

    removed task_instance and task_reschedule from cleanup list

--- dags/dag13_cleanup_pipeline_db.py
+++ dags/dag13_cleanup_pipeline_db.py
@@ -67,13 +67,6 @@ DATABASE_OBJECTS = [
         "keep_last_group_by": DagRun.dag_id
     },
     {
-        "airflow_db_model": TaskInstance,
-        "age_check_column": TaskInstance.execution_date,
-        "keep_last": False,
-        "keep_last_filters": None,
-        "keep_last_group_by": None
-    },
-    {
         "airflow_db_model": Log,
         "age_check_column": Log.dttm,
         "keep_last": False,
@@ -102,17 +95,17 @@ DATABASE_OBJECTS = [
         "keep_last_group_by": None
     }]
 # Check for TaskReschedule model
-try:
-    from airflow.models import TaskReschedule
-    DATABASE_OBJECTS.append({
-        "airflow_db_model": TaskReschedule,
-        "age_check_column": TaskReschedule.execution_date,
-        "keep_last": False,
-        "keep_last_filters": None,
-        "keep_last_group_by": None
-    })
-except Exception as e:
-    logging.error(e)
+# try:
+#     from airflow.models import TaskReschedule
+#     DATABASE_OBJECTS.append({
+#         "airflow_db_model": TaskReschedule,
+#         "age_check_column": TaskReschedule.execution_date,
+#         "keep_last": False,
+#         "keep_last_filters": None,
+#         "keep_last_group_by": None
+#     })
+# except Exception as e:
+#     logging.error(e)
 
 # Check for TaskFail model
 try:
