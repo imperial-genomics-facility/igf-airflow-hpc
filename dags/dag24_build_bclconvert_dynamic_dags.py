307b442ef2b77ab4ab5a79b54a88249542e1acfd
Avik Datta <avik.datta@imperial.ac.uk>
2022-08-10 15:57:53 +0100

    dynamic dag builder dag

--- dags/dag24_build_bclconvert_dynamic_dags.py
+++ dags/dag24_build_bclconvert_dynamic_dags.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+from airflow.models import Variable
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.python import BranchPythonOperator
@@ -7,6 +8,16 @@ from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 from igf_airflow.utils.dag24_build_bclconvert_dynamic_dags_utils import fetch_seqrun_data_from_portal_func
 from igf_airflow.utils.dag24_build_bclconvert_dynamic_dags_utils import format_samplesheet_func
+from igf_airflow.utils.dag24_build_bclconvert_dynamic_dags_utils import generate_dynamic_dag_func
+from igf_airflow.utils.dag24_build_bclconvert_dynamic_dags_utils import copy_dag_to_hpc_func
+from igf_airflow.utils.dag24_build_bclconvert_dynamic_dags_utils import copy_dag_to_remote_func
+from igf_airflow.utils.dag24_build_bclconvert_dynamic_dags_utils import register_pipeline_func
+
+WELLS_DYNAMIC_DAG_DIR = Variable.get('wells_dynamic_dag_dir', default_var=None)
+WELLS_SERVER_HOSTNAME = Variable.get('wells_server_hostname', default_var=None)
+IGF_LIMS_DYNAMIC_DAG_DIR  = Variable.get('igf_lims_dynamic_dag_dir', default_var=None)
+IGF_LIMS_SERVER_HOSTNAME = Variable.get('igf_lims_server_hostname', default_var=None)
+
 
 ## ARGS
 args = {
@@ -44,8 +55,7 @@ with dag:
             params={
                 'samplesheet_info_key': 'samplesheet_info'
             },
-            python_callable=fetch_seqrun_data_from_portal_func,
-        )
+            python_callable=fetch_seqrun_data_from_portal_func)
     ## TASK
     format_samplesheet = \
         PythonOperator(
@@ -64,36 +74,73 @@ with dag:
             python_callable=format_samplesheet_func)
     ## TASK
     generate_dynamic_dag = \
-        DummyOperator(
+        PythonOperator(
             task_id='generate_dynamic_dag',
             dag=dag,
-            queue='hpc_4G'
-        )
+            queue='hpc_4G',
+            params={
+                'formatted_samplesheets_key': 'formatted_samplesheets',
+                'formatted_samplesheets_task': 'format_samplesheet',
+                'sample_groups_key': 'sample_groups',
+                'sample_groups_task': 'format_samplesheet',
+                'samplesheet_info_key': 'samplesheet_info',
+                'samplesheet_info_task': 'fetch_seqrun_data_from_portal',
+                'samplesheet_id_key': 'samplesheet_id',
+                'temp_dag_file_key': 'temp_dag_file',
+                'dag_id_key': 'dag_id'
+            },
+            python_callable=generate_dynamic_dag_func)
     ## TASK
     copy_dag_to_hpc = \
-        DummyOperator(
+        PythonOperator(
             task_id='copy_dag_to_hpc',
             dag=dag,
-            queue='hpc_4G'
-        )
+            queue='hpc_4G',
+            params={
+                'temp_dag_file_key': 'temp_dag_file',
+                'temp_dag_file_task': 'generate_dynamic_dag'
+            },
+            python_callable=copy_dag_to_hpc_func)
     ## TASK
     copy_dag_to_igf_lims = \
-        DummyOperator(
+        PythonOperator(
             task_id='copy_dag_to_igf_lims',
             dag=dag,
-            queue='hpc_4G'
-        )
+            queue='hpc_4G',
+            params={
+                'temp_dag_file_key': 'temp_dag_file',
+                'temp_dag_file_task': 'generate_dynamic_dag',
+                'remote_dynamic_dag_path': IGF_LIMS_DYNAMIC_DAG_DIR,
+                'remote_server_hostname': IGF_LIMS_SERVER_HOSTNAME
+            },
+            python_callable=copy_dag_to_remote_func)
     ## TASK
     copy_dag_to_wells = \
-        DummyOperator(
+        PythonOperator(
             task_id='copy_dag_to_wells',
             dag=dag,
-            queue='hpc_4G'
-        )
+            queue='hpc_4G',
+            params={
+                'temp_dag_file_key': 'temp_dag_file',
+                'temp_dag_file_task': 'generate_dynamic_dag',
+                'remote_dynamic_dag_path': WELLS_DYNAMIC_DAG_DIR,
+                'remote_server_hostname': WELLS_SERVER_HOSTNAME
+            },
+            python_callable=copy_dag_to_remote_func)
     ## TASK
     register_pipeline = \
-        DummyOperator(
+        PythonOperator(
             task_id='register_pipeline',
             dag=dag,
-            queue='hpc_4G'
-        )
+            queue='hpc_4G',
+            params={
+                'dag_id_key': 'dag_id',
+                'dag_id_task': 'generate_dynamic_dag'
+            },
+            python_callable=register_pipeline_func)
+    ## PIPELINE
+    fetch_seqrun_data_from_portal >> format_samplesheet
+    format_samplesheet >> generate_dynamic_dag
+    generate_dynamic_dag >> copy_dag_to_hpc >> register_pipeline
+    generate_dynamic_dag >> copy_dag_to_igf_lims >> register_pipeline
+    generate_dynamic_dag >> copy_dag_to_wells >> register_pipeline
