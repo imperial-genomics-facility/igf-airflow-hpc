e4e1a08d2cebee63e5ae00d54174cfbe14e989df
Avik Datta <avik.datta@imperial.ac.uk>
2023-02-13 14:24:14 +0000

    fix for task dir

--- dags/dag26_snakemake_rnaseq.py
+++ dags/dag26_snakemake_rnaseq.py
@@ -128,7 +128,7 @@ with dag:
             queue='hpc_4G',
             do_xcom_push=False,
             params={
-                "task_key": "snakemake_workdir_key",
+                "task_key": "snakemake_workdir",
                 "task_id": "prepare_snakemake_inputs",
                 "result_dir_name": "results"
             },
