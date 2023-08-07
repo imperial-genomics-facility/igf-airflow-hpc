a8c146799244753a61cbe823629bb058acfd13ca
Avik Datta <avik.datta@imperial.ac.uk>
2023-08-07 21:57:19 +0100

    fix

--- dags/dag33_geomx_processing.py
+++ dags/dag33_geomx_processing.py
@@ -49,33 +49,28 @@ def geomx_dag():
 	analysis_running >> analysis_design
 	analysis_running >> no_work()
 	config_file = \
-		check_and_process_config_file(
-			design_file=analysis_design.design_file)
+		check_and_process_config_file(analysis_design)
 	fastq_list = \
-		fetch_fastq_file_path_from_db(
-			design_file=analysis_design.design_file)
+		fetch_fastq_file_path_from_db(analysis_design)
 	temp_fastq_dir = \
 		create_temp_fastq_input_dir(fastq_list)
 	dcc_run_script = \
 		prepare_geomx_dcc_run_script(
-			design_file=analysis_design.design_file,
+			design_file=analysis_design,
 			symlink_dir=temp_fastq_dir,
 			config_file_dict=config_file)
 	dcc_count = \
-		generate_geomx_dcc_count(
-			dcc_script_dict=dcc_run_script)
+		generate_geomx_dcc_count(dcc_run_script)
 	qc_report = \
 		generate_geomx_qc_report(
-			design_file=analysis_design.design_file,
+			design_file=analysis_design,
 			dcc_count_path=dcc_count)
 	md5_sum = \
 		calculate_md5sum_for_dcc(dcc_count)
 	load_dcc = \
-		load_dcc_count_to_db(
-			dcc_count_path=dcc_count)
+		load_dcc_count_to_db(dcc_count)
 	copy_globus = \
-		copy_data_to_globus(
-			analysis_dir_dict=load_dcc)
+		copy_data_to_globus(load_dcc)
 	copy_globus >> send_email_to_user() >> mark_analysis_finished()
 
 
