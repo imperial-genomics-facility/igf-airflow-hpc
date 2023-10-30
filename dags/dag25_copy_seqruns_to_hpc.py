7eb0adef570cef4b7a1e2beaddadb94d05bac113
Avik Datta <avik.datta@imperial.ac.uk>
2023-10-30 14:23:55 +0000

    generating interop reports

--- dags/dag25_copy_seqruns_to_hpc.py
+++ dags/dag25_copy_seqruns_to_hpc.py
@@ -9,8 +9,10 @@ from airflow.operators.bash import BashOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.ssh.hooks.ssh import SSHHook
 from airflow.operators.empty import EmptyOperator
-from igf_airflow.utils.dag25_copy_seqruns_to_hpc_utils import get_new_run_id_for_copy
-from igf_airflow.utils.dag25_copy_seqruns_to_hpc_utils import register_run_to_db_and_portal_func
+from igf_airflow.utils.dag25_copy_seqruns_to_hpc_utils import (
+    get_new_run_id_for_copy,
+    register_run_to_db_and_portal_func,
+    generate_interop_report_and_upload_to_portal_func)
 
 SEQRUN_SERVER_USER = Variable.get('seqrun_server_user', default_var=None)
 ORWELL_SERVER_HOSTNAME = Variable.get('orwell_server_hostname',  default_var="")
@@ -253,6 +255,21 @@ with dag:
             },
             python_callable=register_run_to_db_and_portal_func)
     ## TASK
+    generate_interop_report_and_upload_to_portal = \
+        PythonOperator(
+            task_id='generate_interop_report_and_upload_to_portal',
+            dag=dag,
+            retry_delay=timedelta(minutes=5),
+            retries=1,
+            queue='hpc_4G',
+            params={
+                'server_in_use': SERVER_IN_USE,
+                'wells_xcom_task': 'get_new_seqrun_id_from_wells',
+                'orwell_xcom_task': 'get_new_seqrun_id_from_orwell',
+                'xcom_key': 'seqrun_id'
+            },
+            python_callable=generate_interop_report_and_upload_to_portal_func)
+    ## TASK
     no_work = \
         EmptyOperator(
             task_id='no_work',
@@ -273,3 +290,4 @@ with dag:
     get_new_seqrun_id_from_orwell>> copy_run_to_orwell
     copy_run_to_orwell >> copy_run_from_orwell_to_hpc
     copy_run_from_orwell_to_hpc >> register_run_to_db_and_portal
+    register_run_to_db_and_portal >> generate_interop_report_and_upload_to_portal
