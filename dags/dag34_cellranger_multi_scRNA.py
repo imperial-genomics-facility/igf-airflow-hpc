97fcdc5841a55f121f3b7f72b07c9fe1078c2196
Avik Datta <avik.datta@imperial.ac.uk>
2024-07-13 00:11:03 +0100

    first deaft

--- dags/dag34_cellranger_multi_scRNA.py
+++ dags/dag34_cellranger_multi_scRNA.py
@@ -1,50 +1,52 @@
-import os, pendulum
-from airflow.decorators import dag, task_group
+import os
+import pendulum
 from airflow.utils.edgemodifier import Label
+from airflow.decorators import dag, task_group
 from igf_airflow.utils.generic_airflow_tasks import (
 	mark_analysis_running,
     fetch_analysis_design_from_db,
-	no_work,
 	send_email_to_user,
 	copy_data_to_globus,
 	mark_analysis_finished,
     create_main_work_dir,
     calculate_md5sum_for_main_work_dir,
-	mark_analysis_failed)
+    load_analysis_results_to_db,
+	mark_analysis_failed,
+    collect_all_analysis)
 from igf_airflow.utils.dag34_cellranger_multi_scRNA_utils import (
     get_analysis_group_list,
     prepare_cellranger_script,
     run_cellranger_script,
     run_single_sample_scanpy,
-    collect_and_branch,
     configure_cellranger_aggr_run,
+    decide_aggr,
     run_cellranger_aggr_script,
     merged_scanpy_report,
     move_single_sample_result_to_main_work_dir,
-    move_aggr_result_to_main_work_dir,
-    load_cellranger_results_to_db)
+    move_aggr_result_to_main_work_dir)
 
-# ## TASK GROUP
+
+## TASK GROUP
 @task_group
-def multiple_sample_task_group(
-    main_work_dir: str,
-    sample_group: str,
-    sample_group_info: dict) -> None:
-    run_info = \
+def prepare_and_run_analysis_for_each_groups(
+        analysis_entry: dict,
+        work_dir: str) -> dict:
+    analysis_script_info = \
         prepare_cellranger_script(
-            sample_group=sample_group,
-            design_dict=sample_group_info)
-    cellranger_output_dir = \
-        run_cellranger_script(run_info)
-    scanpy_output_dict = \
+            analysis_entry=analysis_entry)
+    analysis_output = \
+        run_cellranger_script(
+            analysis_script_info=analysis_script_info)
+    scanpy_out = \
         run_single_sample_scanpy(
-            design_dict=sample_group_info,
-            sample_group=sample_group,
-            cellranger_output_dir=cellranger_output_dir)
-    per_sample_info = \
+            analysis_output=analysis_output)
+    final_output = \
         move_single_sample_result_to_main_work_dir(
-            main_work_dir=main_work_dir,
-            scanpy_output_dict=scanpy_output_dict)
+            analysis_output=scanpy_out,
+            work_dir=work_dir)
+    return final_output
+
+
 ## DAG
 DAG_ID = \
     os.path.basename(__file__).\
@@ -54,60 +56,97 @@ DAG_ID = \
     dag_id=DAG_ID,
 	schedule=None,
 	start_date=pendulum.yesterday(),
-	catchup=False,
+	catchup=True,
 	max_active_runs=10,
     default_view='grid',
     orientation='TB',
     tags=["scrna", "cellranger", "hpc"])
 def cellranger_wrapper_dag():
-    analysis_running = \
+    ## TASK
+    running_analysis = \
         mark_analysis_running(
             next_task="fetch_analysis_design",
-            last_task="no_work")
-    sample_group_info = \
+            last_task="mark_analysis_failed")
+    ## TASK
+    finished_analysis = \
+        mark_analysis_finished()
+    ## TASK
+    failed_analysis = \
+        mark_analysis_failed()
+    ## TASK
+    design = \
         fetch_analysis_design_from_db()
-    main_work_dir = \
-        create_main_work_dir(task_tag='cellranger_multi_output')
-    analysis_running >> Label('Analysis Design found') >> sample_group_info
-    analysis_running >> Label('Analysis Design not found') >> no_work()
-    sample_group_info >> main_work_dir
-    sample_groups = \
+    ## PIPELINE
+    running_analysis >> \
+        Label('Analysis Design found') >> \
+            design
+    running_analysis >> \
+        Label('Analysis Design not found') >> \
+            failed_analysis
+    ## TASK
+    work_dir = \
+        create_main_work_dir(
+            task_tag='cellranger_output')
+    ## PIPELINE
+    design >> work_dir
+    ## TASK
+    analysis_groups = \
         get_analysis_group_list(
-            design_dict=sample_group_info)
-    grp = \
-        multiple_sample_task_group.\
-            partial(
-                main_work_dir=main_work_dir,
-                sample_group_info=sample_group_info).\
-            expand(sample_group=sample_groups)
-    aggr_script_dict = \
-        configure_cellranger_aggr_run()
-    aggr_branch = \
-        collect_and_branch()
-    grp >> aggr_branch
-    aggr_branch >> Label('Multiple samples') >> aggr_script_dict
-    aggr_output_dir = \
+            design_dict=design)
+    ## TASK GROUP
+    analysis_outputs = \
+        prepare_and_run_analysis_for_each_groups.\
+        partial(work_dir=work_dir).\
+        expand(analysis_entry=analysis_groups)
+    ## TASK
+    analysis_output_list = \
+      collect_all_analysis(
+          analysis_outputs)
+    ## TASK
+    aggr_or_not = \
+        decide_aggr(
+            analysis_output_list,
+            aggr_task="",
+            non_aggr_task="")
+    ## TASK
+    aggr_script_info = \
+        configure_cellranger_aggr_run(
+            analysis_output_list)
+    ## TASK
+    aggr_run_dir = \
         run_cellranger_aggr_script(
-           script_dict=aggr_script_dict)
-    scanpy_aggr_output_dict = \
+            aggr_script_info)
+    ## TASK
+    aggr_qc_dir = \
         merged_scanpy_report(
-            design_dict=sample_group_info,
-            cellranger_aggr_output_dir=aggr_output_dir)
-    final_work_dir = \
+            aggr_run_dir)
+    ## TASK
+    aggr_moved = \
         move_aggr_result_to_main_work_dir(
-            main_work_dir=main_work_dir,
-            scanpy_aggr_output_dict=scanpy_aggr_output_dict)
-    md5_file = \
+            work_dir=work_dir,
+            analysis_output_dir=aggr_qc_dir)
+    ## TASK
+    work_dir_with_md5 = \
         calculate_md5sum_for_main_work_dir(
-            main_work_dir=final_work_dir)
-    aggr_branch >> Label('Single sample') >> md5_file
-    loaded_files_info = \
-        load_cellranger_results_to_db(
-            main_work_dir=final_work_dir,
-            md5_file=md5_file)
-    copy_globus = \
-		copy_data_to_globus(loaded_files_info)
-    copy_globus >> send_email_to_user() >> mark_analysis_finished() >> mark_analysis_failed()
+            work_dir)
+    ## PIPELINE
+    aggr_or_not >> aggr_script_info
+    aggr_or_not >> work_dir_with_md5
+    aggr_moved >> work_dir_with_md5
+    ## TASK
+    loaded_data_info = \
+        load_analysis_results_to_db(
+            work_dir_with_md5)
+    ## TASK
+    globus_data = \
+        copy_data_to_globus(
+            loaded_data_info)
+    ## TASK
+    send_email = \
+        send_email_to_user()
+    ## PIPELINE
+    globus_data >> send_email
+    send_email >> failed_analysis
+    send_email >> finished_analysis
 
-##  DAG
 cellranger_wrapper_dag()
