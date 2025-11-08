d3fa25fbf43ff1557cd7cd194b003e85b39eb90e
Avik Datta <avik.datta@imperial.ac.uk>
2025-11-08 13:27:40 +0000

    added DAG doc

--- dags/dag45_metadata_registration.py
+++ dags/dag45_metadata_registration.py
@@ -10,6 +10,12 @@ DAG_ID = (
     .replace(".pyc", "")
     .replace(".py", "")
 )
+doc_md_DAG = """
+### Description
+
+* This DAG checks for an entry of "raw_metadata_id" in the dag_run.conf
+* It fetches the raw metadata entry from protal and registers it in the DB
+"""
 
 @dag(
     dag_id=DAG_ID,
@@ -19,6 +25,7 @@ DAG_ID = (
     max_active_runs=1,
     default_view='grid',
     orientation='TB',
+    doc_md=doc_md_DAG,
     tags=["portal", "metadata", "registration"])
 def dag45_metadata_registration():
     raw_metadata_id = \
