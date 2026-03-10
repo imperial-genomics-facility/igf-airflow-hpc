814840d38b5d9e5f43598dd41f82614c8938e058
Avik Datta <avik.datta@imperial.ac.uk>
2026-03-10 23:28:26 +0000

    added scRNA 10X OCM dag51

---
+++ dags/dag51_scRNA_10X_OCM.py
@@ -0,0 +1,99 @@
+import os
+import pendulum
+from airflow.decorators import dag
+from igf_airflow.utils.generic_airflow_tasks import (
+    mark_analysis_running,
+    fetch_analysis_design_from_db,
+    send_email_to_user,
+    copy_data_to_globus,
+    mark_analysis_finished,
+    create_main_work_dir,
+    calculate_md5sum_for_main_work_dir,
+    load_analysis_results_to_db,
+    mark_analysis_failed)
+from igf_airflow.utils.dag46_scRNA_10X_flex_utils import (
+    prepare_cellranger_flex_script,
+    flex_sample_factory)
+from igf_airflow.utils.dag34_cellranger_multi_scRNA_utils import (
+    run_cellranger_script,
+    run_single_sample_scanpy)
+
+## DAG
+DAG_ID = (
+    os.path.basename(__file__)
+    .replace(".pyc", "")
+    .replace(".py", "")
+)
+
+
+@dag(
+    dag_id=DAG_ID,
+    schedule=None,
+    start_date=pendulum.yesterday(),
+    catchup=False,
+    max_active_runs=10,
+    default_view='grid',
+    orientation='TB',
+    tags=["scRNA", "analysis", "10XGenomics", "OCM"]
+)
+def dag51_scRNA_10X_OCM():
+    ## TASK
+    running_analysis = mark_analysis_running(
+        next_task="fetch_analysis_design",
+        last_task="mark_analysis_failed"
+    )
+    ## TASK
+    design = fetch_analysis_design_from_db()
+    ## TASK
+    work_dir = create_main_work_dir(
+        task_tag='scRNA_OCM_output'
+    )
+    ## TASK - Configure OCM / Flex Pipeline
+    analysis_script_info = prepare_cellranger_flex_script(
+        design_dict=design,
+        work_dir=work_dir
+    )
+    ## TASK - Execute OCM / Flex Pipeline
+    cellranger_output_dir = run_cellranger_script(
+        script_dict=analysis_script_info
+    )
+    ## TASK - fetch flex samples
+    sample_list = flex_sample_factory(
+        cellranger_output_dir=cellranger_output_dir
+    )
+    ## DYNAMIC TASK - Generate Scanpy QC for all samples
+    scanpy_output = (
+        run_single_sample_scanpy
+        .partial(
+            cellranger_output_dir=cellranger_output_dir,
+            design_dict=design
+        )
+        .expand(sample_group=sample_list)
+    )
+    ## TASK
+    work_dir_with_md5 = calculate_md5sum_for_main_work_dir(
+        work_dir
+    )
+    ## TASK
+    loaded_data = load_analysis_results_to_db(
+        work_dir_with_md5
+    )
+    ## TASK
+    globus_data = copy_data_to_globus(
+        loaded_data
+    )
+    ## TASK
+    send_email = send_email_to_user()
+    ## TASK
+    finished_analysis = mark_analysis_finished()
+    ## TASK
+    failed_analysis = mark_analysis_failed()
+    ## PIPELINE
+    running_analysis >> design
+    scanpy_output >> work_dir_with_md5
+    globus_data >> send_email
+    send_email >> finished_analysis
+    send_email >> failed_analysis
+
+
+dag51_scRNA_10X_OCM()
