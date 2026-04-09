927230c7b3353e9e092c2c103f806d64c6fc634b
Avik Datta <avik.datta@imperial.ac.uk>
2026-04-09 21:53:26 +0100

    change hpc mem usage

--- dags/dag23_test_bclconvert_demult.py
+++ dags/dag23_test_bclconvert_demult.py
@@ -160,7 +160,7 @@ with dag:
                 dag=dag,
                 retry_delay=timedelta(minutes=5),
                 retries=1,
-                queue='hpc_16G4t',
+                queue='hpc_32G4t',
                 params={
                     'samplesheet_index': samplesheet_id,
                     'index_column': 'index',
